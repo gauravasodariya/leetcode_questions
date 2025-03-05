f246cc47957a2685ad83ccee92952fcb1bec9d2a
gauravasodariya <gauravasodariya44@gmail.com>
2025-03-05 23:18:50 +0530

    Time: 0 ms (100%), Space: 18 MB (54.78%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,46 +1,59 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) 
+    int findPivot(vector<int>&nums,int n)
     {
-        int low=0,high=nums.size()-1;
-        while(low<=high)
+        int l=0,r=n-1;
+        while(l<r)
         {
-            int mid=(low+high)/2;
+            while(l<r && nums[l]==nums[l+1])
+            {
+                l++;
+            }
+            while(l<r && nums[r]==nums[r-1])
+            {
+                r--;
+            }
+            int mid=l+(r-l)/2;
+            if(nums[mid]>nums[r])
+            {
+                l=mid+1;
+            }
+            else
+            {
+                r=mid;
+            }
+        }
+        return r;
+    }
+    int binarySearch(int l,int r,vector<int>&nums,int target)
+    {
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
             if(nums[mid]==target)
             {
                 return true;
             }
-            if(nums[low]<=nums[mid])
+            else if(nums[mid]<target)
             {
-                if(nums[low]==nums[mid])
-                {
-                    low++;
-                }
-                else if(nums[low]<=target && nums[mid]>=target)
-                {
-                    high=mid-1;
-                }
-                else
-                {
-                    low=mid+1;
-                }
+                l=mid+1;
             }
-            else if(nums[high]>=nums[mid])
+            else
             {
-                if(nums[high]==nums[mid])
-                {
-                    high--;
-                }
-                else if(nums[mid]<=target && nums[high]>=target)
-                {
-                    low=mid+1;
-                }
-                else
-                {
-                    high=mid-1;
-                }
+                r=mid-1;
             }
         }
         return false;
+    }
+    bool search(vector<int>& nums, int target)
+    {
+        int n=nums.size();
+
+        int pivot_index=findPivot(nums,n);
+        if(binarySearch(0,pivot_index-1,nums,target))
+        {
+            return true;
+        }
+        return binarySearch(pivot_index,n-1,nums,target);
     }
 };
