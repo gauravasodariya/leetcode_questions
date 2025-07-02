36a9da1712ea78bff1fab4c272bdf218534e7ccf
gauravasodariya <gauravasodariya44@gmail.com>
2025-07-02 17:28:00 +0530

    Time: 0 ms (100%), Space: 7.9 MB (27.05%) - LeetHub

---
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) 
+    {
+        int xorr=0;
+        for(int i=0;i<n;i++)
+        {
+            xorr=(start+2*i)^xorr;
+        }
+        return xorr;
+    }
+};
