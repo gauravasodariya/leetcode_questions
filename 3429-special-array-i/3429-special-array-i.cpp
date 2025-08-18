9eeee1d0c62d0e990978d1dd0e2599430f6c5b96
gauravasodariya <gauravasodariya44@gmail.com>
2025-08-18 23:18:10 +0530

    Time: 0 ms (100%), Space: 27.8 MB (24.8%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) 
     {
-        if(nums.size()==1)
-        {
-            return true;
-        }
         for(int i=0;i<nums.size()-1;i++)
         {
-            if(nums[i]%2==0 && nums[i+1]%2==0 ||nums[i]%2!=0 && nums[i+1]%2!=0)
+            if((nums[i]&1) == (nums[i+1]&1))
             {
                 return false;
             }
