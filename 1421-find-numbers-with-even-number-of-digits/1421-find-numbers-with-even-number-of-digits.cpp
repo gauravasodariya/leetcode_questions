a580c22b8a38d7b2718618dac960804edf8cfc8b
gauravasodariya <gauravasodariya44@gmail.com>
2025-08-31 11:45:01 +0530

    Time: 0 ms (100%), Space: 14.4 MB (19.01%) - LeetHub

---
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) 
+    {
+        int count=0;
+        for(auto it : nums)
+        {
+            if((to_string(it)).length()%2==0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
