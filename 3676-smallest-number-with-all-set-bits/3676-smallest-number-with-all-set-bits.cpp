e15142f6428212c4bd6e4e37432d47e01835f791
gauravasodariya <gauravasodariya44@gmail.com>
2025-10-29 18:40:02 +0530

    Time: 0 ms (100%), Space: 8.1 MB (99.74%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
+    bool allSetBits(int num){
+        return (num&(num+1))==0;
+    }
     int smallestNumber(int n) 
     {
-        int ans=1;
-        while(ans<n){
-            ans=2*ans+1;
+        int ans=n;
+        while(!allSetBits(ans)){
+            ans++;
         }
         return ans;
     }
