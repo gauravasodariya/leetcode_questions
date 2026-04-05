c6d851af28d001128ca632b3c6f5d61c62864fa9
Asodariya Gaurav Bharatbhai <gauravasodariya44@gmail.com>
2026-04-05 23:11:50 +0530

    Time: 0 ms (100%), Space: 10.3 MB (76.78%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0;
+        int y=0;
+        for(auto ch : moves){
+            if(ch=='U'){
+                y++;
+            }
+            else if(ch=='D'){
+                y--;
+            }
+            else if(ch=='L'){
+                x--;
+            }
+            else{
+                x++;
+            }
+        }
+        if(x==0 && y==0){
+            return true;
+        }
+        return false;
+    }
+};
