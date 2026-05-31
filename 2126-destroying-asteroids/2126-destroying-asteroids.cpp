2df8b8abc58d1926e4451c24fc7cc0e34ab16ffd
Asodariya Gaurav Bharatbhai <gauravasodariya44@gmail.com>
2026-05-31 12:01:41 +0530

    Time: 32 ms (59.12%), Space: 106.8 MB (48.03%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        long long mas=mass;
+        sort(asteroids.begin(),asteroids.end());
+        for(int i=0;i<asteroids.size();i++){
+            if(asteroids[i]<=mas){
+                mas+=asteroids[i];
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
