170c4c0983e01335202367c865e24ad29f7e5a1f
Gaurav Asodariya <gauravasodariya44@gmail.com>
2026-06-17 18:10:26 +0530

    Time: 1 ms (43.19%), Space: 9.1 MB (38.55%) - LeetHub

---
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string greatestLetter(string s) {
+        unordered_set<char>st(s.begin(),s.end());
+        for(char ch='Z';ch>='A';ch--){
+            if(st.count(ch) && st.count(ch+32)){
+                return string(1,ch);
+            }
+        }
+        return "";
+    }
+};
