769b4b7b87b7e20a7d379aec70a0f0dc32a1a982
Gaurav Asodariya <gauravasodariya44@gmail.com>
2026-07-21 21:29:28 +0530

    Time: 103 ms (29.28%), Space: 81 MB (32.43%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        int activeCount(count(s.begin(),s.end(),'1'));
+        vector<int>inactiveBlocks;
+        int i=0;
+        while(i<s.size()){
+            if(s[i]=='0'){
+                int start=i;
+                while(i<s.size() && s[i]=='0'){
+                    i++;
+                }
+                inactiveBlocks.push_back(i-start);
+            }
+            else{
+                i++;
+            }
+        }
+        int maxPairSum=0;
+        for(int i=1;i<inactiveBlocks.size();i++){
+            maxPairSum=max(maxPairSum,inactiveBlocks[i]+inactiveBlocks[i-1]);
+        }
+        return maxPairSum+activeCount;
+    }
+};
