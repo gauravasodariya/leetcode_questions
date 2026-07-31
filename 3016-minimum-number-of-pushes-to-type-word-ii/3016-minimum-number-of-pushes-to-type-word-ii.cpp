b90ef6c1accd732e24bb39f4c980345a39dd9fb3
Gaurav Asodariya <gauravasodariya44@gmail.com>
2026-07-31 09:35:45 +0530

    Time: 9 ms (53.64%), Space: 25.6 MB (61.34%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int>freq(26,0);
+        for(int i=0;i<word.size();i++){
+            freq[word[i]-'a']++;
+        }
+        sort(freq.begin(),freq.end(),greater<int>());
+        int ans=0;
+        for(int i=0;i<26;i++){
+            if(freq[i]==0){
+                break;
+            }
+            int push=i/8+1;
+            ans+=freq[i]*push;
+        }
+        return ans;
+    }
+};
