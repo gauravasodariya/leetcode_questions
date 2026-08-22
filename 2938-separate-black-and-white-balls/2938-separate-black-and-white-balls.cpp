9d15031b1d12bb827f6ad0bd159f08af0d00cfe0
Gaurav Asodariya <gauravasodariya44@gmail.com>
2026-08-22 11:28:07 +0530

    Time: 6 ms (58.01%), Space: 17.2 MB (73.35%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long black=0;
+        long long ans=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='1'){
+                black++;
+            }
+            else{
+                ans+=black;
+            }
+        }
+        return ans;
+    }
+};
